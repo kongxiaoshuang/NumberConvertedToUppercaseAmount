ddce2b39beabce4fb53260a4aa7d040fa795b402
kongxiaoshuang <Charlie.kong@5imakeup.com>
2017-01-25 14:06:52 +0800

    初始化项目

---
+++ src/EcanRMB/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcanRMB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Console.Write("请输入金额：\n");
+            input = Console.ReadLine().ToString();
+           string result= CapitalHelper.CmycurD(input);
+            Console.WriteLine(result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
